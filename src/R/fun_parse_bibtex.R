9d156b4e891d30324f1587c47f920936b81b334a
Brandon <brandon.monier@gmail.com>
2022-01-30 15:47:18 -0500

    Finalize citation parser

---
+++ src/R/fun_parse_bibtex.R
@@ -0,0 +1,130 @@
+# Functions for parsing BibTex file ---------------------------------
+
+
+`%>%` <- magrittr::`%>%`
+
+fieldCollector <- function(field, rawText) {
+    ifelse(
+        test = length(rawText[grep(field, rawText)]) == 0,
+        yes  = return(NA),
+        no   = return(rawText[grep(field, rawText)])
+    )
+}
+
+parseBibtex <- function(citationUrl = bibFile) {
+    ## Get raw text ----
+    rawBibtex <- readLines(
+        con = citationUrl
+    )
+
+    ## Split into chunks ----
+    articles <- list()
+    count <- 1
+    j <- 0
+    for (i in rawBibtex) {
+        if (grepl("^@", i)) {
+            k <- 2
+            j <- j + 1
+            articles[[j]] <- i
+        } else {
+            articles[[j]][k] <- i
+            k <- k + 1
+        }
+    }
+
+    ## Convert to data frame ----
+    articleDf <- data.frame(
+        title   = "",
+        year    = "",
+        journal = "",
+        authors = "",
+        type    = "",
+        doi     = ""
+    )
+    j <- 1
+    for (i in articles) {
+        articleDf[j, ]$title   <- fieldCollector("title=", i)
+        articleDf[j, ]$year    <- fieldCollector("year=", i)
+        articleDf[j, ]$journal <- fieldCollector("journal=", i)
+        articleDf[j, ]$authors <- fieldCollector("author=", i)
+        articleDf[j, ]$type    <- fieldCollector("@", i)
+        articleDf[j, ]$doi     <- fieldCollector("doi=", i)
+        j <- j + 1
+    }
+
+    ## Clean up ----
+    articleDf$title   <- gsub("title=\\{|\\}(,|$)",   "", articleDf$title) %>% trimws()
+    articleDf$year    <- gsub("year=\\{|\\}(,|$)",    "", articleDf$year) %>% trimws() %>% as.numeric()
+    articleDf$journal <- gsub("journal=\\{|\\}(,|$)", "", articleDf$journal) %>% trimws()
+    articleDf$authors <- gsub("author=\\{|\\}(,|$)",  "", articleDf$author) %>%
+        trimws() %>%
+        gsub(" and ", "; ", .) %>%
+        gsub("\\{\\\\\"a\\}", "a", .) %>%
+        gsub("\\{\\\\\"o\\}", "o", .) %>%
+        gsub("\\{\\\\\"u\\}", "u", .)
+    articleDf$type    <- gsub("@|\\{.*", "", articleDf$type)
+    articleDf$doi     <- gsub("doi=\\{|\\}(,|$)",   "", articleDf$doi) %>% trimws()
+
+    articleDf <- articleDf[!is.na(articleDf$year), ]
+    articleDf <- articleDf[order(-articleDf$year), ]
+    row.names(articleDf) <- NULL
+
+    ## HTML ----
+    articleDf$authors <- gsub("Monier, Brandon", "<b>Monier, Brandon</b>", articleDf$authors)
+    articleDf$journal <- ifelse(
+        test = is.na(articleDf$journal),
+        yes = NA,
+        no = paste0("<i>", articleDf$journal, "</i>")
+    )
+    articleDf$journal[articleDf$type == "unpublished"] <- "<i>In preparation</i>"
+
+    return(articleDf)
+}
+
+
+synDf <- c(
+    "article"     = "Journal Articles",
+    "book"        = "Proceedings and Book Chapters",
+    "unpublished" = "In preparation",
+    "thesis"      = "Theses"
+)
+
+
+sectionParser <- function(
+    sub     = "article",
+    synonym = synDf,
+    rawData = rawDf
+) {
+    tmpHtml <- paste0("<h2>", synonym[[sub]], "</h2>")
+
+
+    if (sub == "thesis") {
+        tmpDf <- rawData[rawData$type == "phdthesis" | rawData$type == "mastersthesis", ]
+    } else {
+        tmpDf <- rawData[rawData$type == sub, ]
+    }
+
+    tmpDf %<>%
+        split(., f = .$year) %>%
+        rev()
+
+    tmpLs <- vector("list", tmpDf %>% length())
+    for (i in tmpDf %>% length() %>% seq_len()) {
+        for (j in tmpDf[[i]] %>% nrow() %>% seq_len()) {
+            tmpLs[[i]][1] <- paste0("<h3>", names(tmpDf)[i], "</h3>")
+            tmpLs[[i]][j + 1] <- paste0(
+                "<p>",
+                tmpDf[[i]][j, ]$authors,
+                " (", tmpDf[[i]][j, ]$year, ") ",
+                tmpDf[[i]][j, ]$title, ". ",
+                tmpDf[[i]][j, ]$journal,
+                " doi: <a class=\"body\" href=\"", tmpDf[[i]][j, ]$doi, "\">", tmpDf[[i]][j, ]$doi, "</a>",
+                "</p>"
+            )
+        }
+    }
+
+    return(c(tmpHtml, tmpLs %>% unlist()))
+}
+
+
--- src/R/parse_bibtex.R
+++ src/R/parse_bibtex.R
@@ -1,98 +1,32 @@
-library(magrittr)
+# library(magrittr)
+
+source("src/R/fun_parse_bibtex.R")
+
+headTemplate <- "assets/templates/head.html"
+tailTemplate <- "assets/templates/tail.html"
+bibFile      <- "assets/publications.bib"
+outputFile   <- "publications.html"
 
 
-parseBibtex <- function(citationUrl = "assets/publications.bib") {
-    ## Get raw text ----
-    rawBibtex <- readLines(
-        con = citationUrl
-    )
-    
-    
-    ## Split into chunks ----
-    articles <- list()
-    count <- 1
-    j <- 0
-    for (i in rawBibtex) {
-        if (grepl("^@", i)) {
-            k <- 2
-            j <- j + 1
-            articles[[j]] <- i
-        } else {
-            articles[[j]][k] <- i
-            k <- k + 1
-        }
-    }
-    
-    
-    ## Convert to data frame ----
-    articleDf <- data.frame(
-        title   = "",
-        year    = "",
-        journal = "",
-        authors = "",
-        type    = ""
-    )
-    j <- 1
-    for (i in articles) {
-        articleDf[j, ]$title   <- ifelse(
-            test = length(i[grep("title=", i)]) == 0, 
-            yes  = NA, 
-            no   = i[grep("title=", i)]
-        )
-        articleDf[j, ]$year    <- ifelse(
-            test = length(i[grep("year=", i)]) == 0, 
-            yes  = NA, 
-            no   = i[grep("year=", i)]
-        )
-        articleDf[j, ]$journal <- ifelse(
-            test = length(i[grep("journal=", i)]) == 0, 
-            yes  = NA, 
-            no   = i[grep("journal=", i)]
-        )
-        articleDf[j, ]$authors <- ifelse(
-            test = length(i[grep("author=", i)]) == 0, 
-            yes  = NA, 
-            no   = i[grep("author=", i)]
-        )
-        articleDf[j, ]$type <- ifelse(
-            test = length(i[grep("@", i)]) == 0, 
-            yes  = NA, 
-            no   = i[grep("@", i)]
-        )
-        j <- j + 1
-    }
-    
-    
-    ## Clean up ----
-    articleDf$title   <- gsub("title=\\{|\\},",   "", articleDf$title) %>% trimws()
-    articleDf$year    <- gsub("year=\\{|\\}(,|$)",    "", articleDf$year) %>% trimws() %>% as.numeric()
-    articleDf$journal <- gsub("journal=\\{|\\},", "", articleDf$journal) %>% trimws()
-    articleDf$authors <- gsub("author=\\{|\\},",  "", articleDf$author) %>%
-        trimws() %>%
-        gsub(" and ", "; ", .) %>%
-        gsub("\\{\\\\\"a\\}", "a", .) %>% 
-        gsub("\\{\\\\\"o\\}", "o", .) %>% 
-        gsub("\\{\\\\\"u\\}", "u", .)
-    articleDf$type    <- gsub("@|\\{.*", "", articleDf$type)
-    
-    articleDf <- articleDf[!is.na(articleDf$year), ]
-    articleDf <- articleDf[order(-articleDf$year), ]
-    row.names(articleDf) <- NULL
-    
-    
-    ## HTML ----
-    articleDf$authors <- gsub("Monier, Brandon", "<b>Monier, Brandon</b>", articleDf$authors)
-    articleDf$journal <- ifelse(
-        test = is.na(articleDf$journal),
-        yes = NA,
-        no = paste0("<i>", articleDf$journal, "</i>")
-    )
-    
-    return(articleDf)
-}
+rawDf <- parseBibtex()
 
-
-parseBibtex() -> articleDf
-
+writeLines(
+    text = c(
+        readLines(headTemplate),
+        "<h1>Publications</h1>",
+        sectionParser("unpublished"),
+        sectionParser("book"),
+        sectionParser("article"),
+        sectionParser("thesis"),
+        "<br>",
+        paste0(
+            "<p><i>Last Compiled: </i><code>",
+            format(Sys.Date(), '%Y-%m-%d'),
+            "</code></p>"
+        ),
+        readLines(tailTemplate)
+    ),
+    con = outputFile
+)
 
 
